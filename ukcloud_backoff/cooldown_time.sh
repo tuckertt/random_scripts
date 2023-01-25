2350cda9ed906c60bc21f897f78ca89bb630cb82
Richard Byrne <40005310+tuckertt@users.noreply.github.com>
2023-01-25 14:08:52 +0000

    Update cooldown_time.sh

--- ukcloud_backoff/cooldown_time.sh
+++ ukcloud_backoff/cooldown_time.sh
@@ -5,9 +5,12 @@ source $PWD/secured_items.txt
 cooldown_seconds=$( curl -i -H "Authorization: token $git_auth_token" https://api.github.com/user | grep x-ratelimit-remaining | rep -o '[[:num:]]')
 current_seconds=$(date +%s)
 
-cooldown_expiry_seconds=$(( $cooldown_seconds + $current_seconds ))
+cooldown_info=$( curl -i -H "Authorization: token $git_auth_token" https://api.github.com/user | grep ratelimit )
 
+cooldown_expiry_seconds=$(( $cooldown_seconds + $current_seconds ))
 
 cooldown_time=$( date -d @$cooldown_expiry_seconds "+%Y-%m-%d %H:%M")
 
 echo "Github reports that continuation can occur at \"$cooldown_time\" "
+echo "cooldown info"
+echo "$cooldown_info"
